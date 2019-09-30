5d0eafe18eb85faf3fe68c631100b0182e53296f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-09-30 19:56:55 +0530

    Update user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
-var mongoose=require("mongoose"),
-    passportLocalMongoose=require("passport-local-mongoose");
+const mongoose            = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
-var userSchema=new mongoose.Schema({
+const userSchema=new mongoose.Schema({
       username:String,
       password:String
 });
+
 userSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("User",userSchema);
