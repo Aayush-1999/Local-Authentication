685acf5c2a2be0970de6cedad79c2a89d932c8d6
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-09-30 19:56:05 +0530

    Update routes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express=require("express"),
-    mongoose=require("mongoose"),
-    bodyParser=require("body-parser"),
-    User=require("./models/user")
-    passport=require("passport"),
-    passportLocal=require("passport-local"),
-    passportLocalMongoose=require("passport-local-mongoose"),
-    expressSession=require("express-session");
+const express               = require("express"),
+      mongoose              = require("mongoose"),
+      bodyParser            = require("body-parser"),
+      User                  = require("./models/user")
+      passport              = require("passport"),
+      passportLocal         = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose"),
+      expressSession        = require("express-session"),
+      app                   = express();
 
-var app=express();
 mongoose.connect("mongodb://127.0.0.1/auth-demo-app",{useNewUrlParser:true});
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,37 +17,36 @@ app.use(expressSession({
       resave:false,
       saveUninitialized:false
 }));
+
+//PASPORT CONFIGURATION
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/",function(req,res){
+app.get("/",(req,res)=>{
      res.render("home");
 });
 
-app.get("/secret", isloggedin ,function(req,res){
+app.get("/secret", isloggedin ,(req,res)=>{
     res.render("secret");
 });
 
-app.get("/register",function(req,res){
+app.get("/register",(req,res)=>{
     res.render("register");
 });
 
-app.post("/register",function(req,res){
-    User.register(new User({username:req.body.username}),req.body.password,function(err,user){
-        if(err){
-            console.log(err);
-            res.render("register");
-        }
+app.post("/register",(req,res)=>{
+    User.register(new User({username:req.body.username}),req.body.password,(err,user)=>{
+        if(err) res.render("register");
         passport.authenticate("local")(req,res,function(){
             res.redirect("/secret");
         });
     });
 });
 
-app.get("/login",function(req,res){
+app.get("/login",(req,res)=>{
     res.render("login");
 });
 app.post("/login",passport.authenticate("local",{
@@ -56,11 +55,12 @@ app.post("/login",passport.authenticate("local",{
 }),function(req,res){
 });
 
-app.get("/logout",function(req,res){
+app.get("/logout",(req,res)=>{
     req.logout();
     res.redirect("/");
 })
 
+//MIDLEWARE
 function isloggedin(req, res,next){
     if(req.isAuthenticated()){
        return next();
